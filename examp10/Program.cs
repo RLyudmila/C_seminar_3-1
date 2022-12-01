aff6fbf79386a1acfd813775b3fe5bee795cd010
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-12-01 13:59:55 +0300

    Polindrom

--- examp10/Program.cs
+++ examp10/Program.cs
@@ -1,12 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. Не использовать String!
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
 
-int a;
-a = 12821;
-a  = a/1000;
-a = a% 10;
+
+ int number = int.Parse(Console.ReadLine());
+
+int number1 = number/1000;
+int number2 = number%100;
+int first = number1/10;
+int last = number2%10;
+int second = number1%10;
+int penultimate = number2/10;
+if ((first == last) & (second == penultimate))
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else
+{
+    Console.WriteLine("Число НЕ является полиндромом");
+}
+
+
+
